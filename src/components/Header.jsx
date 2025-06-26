09fdfc3e07e71a540d15eba8d474dd0d4b07293e
italoesilva <italoliveira95@hotmail.com>
2025-06-26 20:34:23 -0300

    mudando o visual

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
+import './Header.css';
+
 function Header() {
   return (
-    <header style={{
-      backgroundColor: '#333',
-      color: 'white',
-      padding: '30px',
-      textAlign: 'center'
-    }}>
-      <h1>Ítalo Oliveira</h1>
-      <p>Desenvolvedor Front-End em formação</p>
+    <header className="header">
+      <div className="header-content">
+        <h1>Ítalo Oliveira</h1>
+        <p>Desenvolvedor Front-End em formação</p>
+      </div>
     </header>
   );
 }
