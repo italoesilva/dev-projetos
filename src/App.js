8501ae8229fb014d339f8dc3d874a715b86b5aee
italoesilva <italoliveira95@hotmail.com>
2025-06-26 21:03:41 -0300

    lista de tarefas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import ProjectList from './components/ProjectList';
 import TodoList from './components/TodoList';
+import Calculator from './components/Calculator'; // seu componente da calculadora
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<ProjectList />} />
+        <Route path="/calculadora" element={<Calculator />} />
         <Route path="/tarefas" element={<TodoList />} />
       </Routes>
     </HashRouter>
