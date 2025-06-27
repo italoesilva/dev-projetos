9331cd236ddc52853975ce432253916a561ce4ea
italoesilva <italoliveira95@hotmail.com>
2025-06-26 21:02:02 -0300

    lista de tarefas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import ProjectList from './components/ProjectList';
 import TodoList from './components/TodoList';
-import Calculator from './components/Calculator';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<ProjectList />} />
         <Route path="/tarefas" element={<TodoList />} />
-        <Route path="/calculadora" element={<Calculator />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
